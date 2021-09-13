d2178b2335926a858c353f8de662e102daee6513
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-09-13 14:09:25 -0500

    Add files via upload

---
+++ js/actualizaciones.js
@@ -0,0 +1,33 @@
+var myObject = {
+    "publicacionesXiiber": [{
+        "fechaDePublicacion": "14-agosto-2020",
+        "Post": "Hice tal cosa",
+        "enlace": "https://www.xiiber.com/index.html",
+        "href": "https://www.xiiber.com/index.html",
+        "data-href": "index",
+        "img-avatar": "viajes",
+        "pageid": "viajes",
+        "accion": "Descargar"
+
+    }, {
+        "fechaDePublicacion": "15-agosto-2020",
+        "Post": "Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa ",
+        "enlace": "https://www.xiiber.com",
+        "href: ": "https: //tepozspa.xiiber.com",
+        "data-href": "xiiber",
+        "img-avatar": "https://www.xiiber.com/favicon.png",
+        "pageid": "viajes",
+        "accion": "leer"
+
+    }, {
+        "fechaDePublicacion": "16-agosto-2020",
+        "Post": "Y acabe",
+        "enlace": "leer ahora",
+        "href: ": "www.xiiber.com",
+        "img-avatar": "https://www.xiiber.com/favicon.png",
+        "pageid": "viajes",
+        "data-href": "mapadesitio",
+        "id": "mapadesitio",
+        "accion": "ir"
+    }]
+};
